f118fc6e7c717a2c49855154fba6d27b0772852e
rremii <88894948+rremii@users.noreply.github.com>
2023-03-23 22:54:19 +0300

    making header layout

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,14 +1,42 @@
 import styled from "styled-components"
+import Settings from "@shared/assets/DarkTheme/settings.svg"
+// import Logo from '@shared/assets/DarkTheme/l'
 
 const Header = () => {
 
   return <HeaderLayout>
-    header
+    {/*<img alt='logo' className="logo" src={}/>*/}
+    <span></span>
+    <div className="logo">
+      Quick Brains
+    </div>
+    <div className="settings">
+      <img src={Settings} alt="settings" />
+    </div>
   </HeaderLayout>
 }
 export default Header
 const HeaderLayout = styled.header`
   height: 74px;
   background-color: #806690;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 22px;
+
+  .logo {
+    font-family: 'Inter', sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 32px;
+    line-height: 39px;
+    -webkit-text-stroke: 1px #4D4363;
+    color: rgba(254, 254, 254, 0.62);
+  }
+
+  .settings {
+
+  }
 
 `
